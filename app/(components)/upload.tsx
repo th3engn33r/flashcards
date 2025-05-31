efe65954d82db436f4854a3637a2b52ed508122d
engineer <shayan121@hotmail.com>
2025-05-31 22:25:46 +0200

    add upload and clear button to file upload component

--- app/(components)/upload.tsx
+++ app/(components)/upload.tsx
@@ -13,20 +13,36 @@ const Upload = () => {
   return (
     <>
       <div className='flex bg-gray-400 rounded-2xl border-4 border-gray-700 text-white text-center items-center justify-center h-[50vh]'>
-        <div className='flex flex-col rounded border-dashed border-4 text-white border-white w-80 h-60 text-center justify-center'>
-          <input
-            className=' h-[20vh] text-gray-400 '
-            type='file'
-            onChange={handleUpload}
-          />
-          <p className=''>click to upload file</p>
-        </div>
+        {!file && (
+          <div className='flex flex-col rounded border-dashed border-4 text-white border-white w-80 h-60 text-center justify-center'>
+            <input
+              className=' h-[20vh] text-gray-400 '
+              type='file'
+              onChange={handleUpload}
+            />
+            <p className=''>click to upload file</p>
+          </div>
+        )}
+        {file && (
+          <div className='text-center my-3'>
+            <p className='mb-8'>File selected: {file.name}</p>
+            <button
+              className='bg-blue-700 mx-2 w-30 h-10 rounded-2xl'
+              onClick={() => redirect('/upload')}
+            >
+              {' '}
+              Upload
+            </button>
+            <button
+              className='bg-blue-700 mx-2 w-30 h-10 rounded-2xl'
+              onClick={() => setFile(null)}
+            >
+              {' '}
+              Clear
+            </button>
+          </div>
+        )}
       </div>
-      {file && (
-        <div className='text-center my-3'>
-          <p>File selected: {file.name}</p>
-        </div>
-      )}
     </>
   );
 };
